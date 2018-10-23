9c9ad04b483088c9a293633fa6a31658775101dd
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2018-10-23 09:28:19 +0200

    TTK-19508 create new plugin integratedDuration

---
+++ plugins/es.teltek.paella.integratedDurationPlugin/integratedDuration.js
@@ -0,0 +1,61 @@
+
+paella.addPlugin(function() {
+	return class integratedDurationPlugin extends paella.ButtonPlugin {
+		getAlignment() { return 'left'; }
+		getSubclass() { return 'integratedDurationButton'; }
+		getName() { return "es.teltek.paella.integratedDurationPlugin"; }
+		getDefaultToolTip() { return base.dictionary.translate("Duration"); }
+		getIndex() {return 141;}
+
+		checkEnabled(onSuccess) {
+			onSuccess(true);
+		}
+
+		setup() {
+		    let self = this;
+			paella.events.bind(paella.events.timeUpdate,function(event,params) {self.changeTime();});
+		}
+
+		changeTime(){
+		    let thisClass = this;
+		    
+            paella.player.videoContainer.currentTime().then(function(duration) {
+                paella.player.videoContainer.duration().then(function(d) {
+                      duration = parseInt(duration);
+
+                      thisClass.setText(thisClass.secondsToHours(duration) + " / " + thisClass.secondsToHours(parseInt(d)));
+                });
+		    });
+        }
+
+		buildContent(domElement) {
+            let durationContainer = document.createElement('div');
+            durationContainer.className = 'durationContainer';
+            domElement.appendChild(durationContainer);
+        }
+
+        getText() {
+            return "";
+        }
+
+        secondsToHours(sec_numb) {
+            let hours   = Math.floor(sec_numb / 3600);
+            let minutes = Math.floor((sec_numb - (hours * 3600)) / 60);
+            let seconds = sec_numb - (hours * 3600) - (minutes * 60);
+
+            if (minutes < 10) {minutes = "0"+minutes;}
+            if (seconds < 10) {seconds = "0"+seconds;}
+
+            if(hours === 0) {
+                return minutes + ':' + seconds;
+            }
+
+            if(minutes === 0) {
+                return '0:' + seconds;
+            }
+
+            return hours + ':' + minutes + ':' + seconds;
+        }
+
+	};
+});
